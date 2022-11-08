c53f435196a57e32601cf44e2cb1902dc1878044
maeng <ggmaeng@gmail.com>
2022-11-08 17:35:36 +0900

    add

---
+++ example/dict.py
@@ -0,0 +1,15 @@
+n_list = ['최원칠','이서혁','aiot','새싹']
+age_list = [30,29,34,72]
+id_list = [125,2474,341,646]
+
+d_list = []
+
+~
+
+print(d_list)
+total_age = 0
+~
+print(total_age)
+
+~
+print(d_list[3]) # 새싹, 35가 출력
