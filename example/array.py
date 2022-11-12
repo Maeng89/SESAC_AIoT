220ef8ad3b7466e47f31d6792f20cc97de0267b4
maeng <ggmaeng@gmail.com>
2022-11-12 09:02:45 +0900

    add

---
+++ example/array.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+mat3 = [1,2,'a']
+~
+print(mat3, type(mat3))
+
+rand1 = np.random.rand(5,10)
+~
+print(sub_rand1.shape)
+
+matrix = [
+     [1, 2, 3, 4],
+     [5, 6, 7, 8],
+     [9, 10, 11, 12]
+]
+
+amat = np.asarray(matrix)
+~
+print(max_mat1)
+~
+print(max_mat2)
+
+
+rand2 = np.random.rand(5,6)
+~
+print(cat_rand.shape)
